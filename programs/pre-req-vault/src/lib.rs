a09d070ddd8f4bfeba8f46c1f5a365235295f563
Gideon Okorie <giditech14.com>
2026-08-19 08:13:18 +0100

    updated the program id by syncing

--- programs/pre-req-vault/src/lib.rs
+++ programs/pre-req-vault/src/lib.rs
@@ -9,7 +9,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("DBoobRVqT7PaAhq8obqo95723CHyWvcCLRUKvQ5wYWE4");
+declare_id!("DmKawJ8SSzcLUxzoH2LEoM53NjWUdbyzFz2C9RdvayC5");
 
 #[program]
 pub mod pre_req_vault {
